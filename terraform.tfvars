be49cfddf28375d67b4092938fd0d6a0cd635fe1
Akhil Tidke <akhiltidke@Mac.bwrouter>
2026-08-22 12:05:06 +0530

    first commit

---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+aws_region         = "ap-south-1"
+project_name       = "jenkins-terraform-demo"
+
+vpc_cidr           = "10.0.0.0/16"
+public_subnet_cidr = "10.0.1.0/24"
+
+availability_zone  = "ap-south-1a"
+
+instance_type      = "t3.micro"
+
+ami_id             = "YOUR_UBUNTU_AMI_ID"
+
+my_ip              = "YOUR_PUBLIC_IP/32"
---
+++ variable.tf
@@ -0,0 +1,45 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "jenkins-terraform-demo"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "Public subnet CIDR"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability Zone"
+  type        = string
+  default     = "ap-south-1a"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "Ubuntu AMI ID"
+  type        = string
+}
+
+variable "my_ip" {
+  description = "Your public IP in CIDR notation"
+  type        = string
+}
